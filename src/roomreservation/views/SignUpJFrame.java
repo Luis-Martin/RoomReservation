4f18ddd17efaee0a4fd50f3b3029f555ca21eb39
Martin <luis.aaron.mp17@gmail.com>
2024-11-13 04:18:54 -0500

    Agregar degradado de fondo en SignUpJFrame

--- src/roomreservation/views/SignUpJFrame.java
+++ src/roomreservation/views/SignUpJFrame.java
@@ -10,6 +10,9 @@ import roomreservation.controller.UserController;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GradientPaint;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -43,7 +46,17 @@ public class SignUpJFrame extends javax.swing.JFrame {
         setTitle("SignUp");
 
         // creacion de ventana
-        JPanel panel = new JPanel(); 
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                // Degradado de color vertical desde #E7E6E6 (arriba) hasta #D4FEE4 (abajo)
+                GradientPaint gradient = new GradientPaint(0, 0, Color.decode("#E7E6E6"), 0, getHeight(), Color.decode("#D4FEE4"));
+                g2d.setPaint(gradient);
+                g2d.fillRect(0, 0, getWidth(), getHeight());
+            }
+        }; 
         panel.setLayout(new GridBagLayout());
         GridBagConstraints constraints;
 
