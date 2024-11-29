3cf754861e93d7d6c73b902fec4e28e4c1993e16
Martin <luis.aaron.mp17@gmail.com>
2024-11-29 15:12:03 -0500

    Agregar Frame de perfil

--- src/roomreservation/views/ProfileJFrame.java
+++ src/roomreservation/views/ProfileJFrame.java
@@ -7,25 +7,27 @@ package roomreservation.views;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.border.LineBorder;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import roomreservation.components.MenuBar;
 import roomreservation.RoomReservation;
+import roomreservation.controller.UserController;
+import roomreservation.model.User;
 /**
  *
  * @author USUARIO
  */
 public class ProfileJFrame extends javax.swing.JFrame {
-
-    /**
-     * Creates new form MiCuenta
-     */
-    
-    Color mColorFondo = new Color(18, 54, 41);
-    
     public ProfileJFrame() {
+        System.out.println(RoomReservation.loggedInUser);
         initComponents();
         setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
         setTitle("Perfil");
@@ -34,17 +36,156 @@ public class ProfileJFrame extends javax.swing.JFrame {
         MenuBar menuBar = new MenuBar(this);  // Pasamos 'this' para que el menú conozca el JFrame actual
         setJMenuBar(menuBar.getMenuBar());  // Configura el JMenuBar en el JFrame
 
-        // Colocar el resto del código para el JFrame
-        System.out.println("Usuario actual: " + RoomReservation.loggedInUser);
+        // Crear el panel principal
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        
+        // Título
+        JLabel titleLabel = new JLabel("Mi cuenta");
+        titleLabel.setFont(new Font("Andale Mono", Font.BOLD, 24));
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.CENTER;
+        constraints.insets = new java.awt.Insets(0, 0, 40, 0); // Márgenes
+        panel.add(titleLabel, constraints);
+        
+        // Etiqueta de "Nombre"
+        JLabel nameLabel = new JLabel("Nombre:");
+        nameLabel.setFont(new Font("Andale Mono", Font.PLAIN, 15));
+        constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.insets = new Insets(10, 0, 10, 10);
+        constraints.anchor = GridBagConstraints.WEST;
+        panel.add(nameLabel, constraints);
 
-    }
-      private JMenuItem createMenuItem(String text, Font font) {
-        JMenuItem menuItem = new JMenuItem(text);
-        menuItem.setOpaque(true);
-        menuItem.setFont(font);
-        menuItem.setForeground(Color.white);
-        menuItem.setBackground(mColorFondo);
-        return menuItem;
+        // Campo de texto de nombre
+        JTextField nameField = new JTextField();
+        nameField.setPreferredSize(new Dimension(273, 38));
+        nameField.setFont(new Font("Andale Mono", Font.PLAIN, 15));
+        nameField.setForeground(Color.BLACK);
+        nameField.setText(RoomReservation.loggedInUser.getName());
+        constraints = new GridBagConstraints();
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.insets = new Insets(10, 0, 10, 40);
+        panel.add(nameField, constraints);
+        
+        // Etiqueta de "Correo"
+        JLabel emailLabel = new JLabel("Correo:");
+        emailLabel.setFont(new Font("Andale Mono", Font.PLAIN, 15));
+        constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.insets = new Insets(10, 0, 10, 10);
+        constraints.anchor = GridBagConstraints.WEST;
+        panel.add(emailLabel, constraints);
+
+        // Campo de texto de correo
+        JTextField emailField = new JTextField();
+        emailField.setPreferredSize(new Dimension(273, 38));
+        emailField.setFont(new Font("Andale Mono", Font.PLAIN, 15));
+        emailField.setForeground(Color.BLACK);
+        emailField.setText(RoomReservation.loggedInUser.getEmail());
+        constraints = new GridBagConstraints();
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.insets = new Insets(10, 0, 10, 40);
+        panel.add(emailField, constraints);
+        
+        // Etiqueta de "Celular"
+        JLabel phoneLabel = new JLabel("Celuar:");
+        phoneLabel.setFont(new Font("Andale Mono", Font.PLAIN, 15));
+        constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.insets = new Insets(10, 0, 10, 10);
+        constraints.anchor = GridBagConstraints.WEST;
+        panel.add(phoneLabel, constraints);
+
+        // Campo de texto de celular
+        JTextField phoneField = new JTextField();
+        phoneField.setPreferredSize(new Dimension(273, 38));
+        phoneField.setFont(new Font("Andale Mono", Font.PLAIN, 15));
+        phoneField.setForeground(Color.BLACK);
+        phoneField.setText(RoomReservation.loggedInUser.getPhone());
+        constraints = new GridBagConstraints();
+        constraints.gridx = 1;
+        constraints.gridy = 3;
+        constraints.insets = new Insets(10, 0, 10, 40);
+        panel.add(phoneField, constraints);
+        
+        // Etiqueta de "Celular"
+        JLabel newPasswordLabel = new JLabel("Nueva contraseña:");
+        newPasswordLabel.setFont(new Font("Andale Mono", Font.PLAIN, 15));
+        constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 4;
+        constraints.insets = new Insets(10, 0, 10, 10);
+        constraints.anchor = GridBagConstraints.WEST;
+        panel.add(newPasswordLabel, constraints);
+
+        // Campo de texto de celular
+        JTextField newPasswordField = new JTextField();
+        newPasswordField.setPreferredSize(new Dimension(273, 38));
+        newPasswordField.setFont(new Font("Andale Mono", Font.PLAIN, 15));
+        newPasswordField.setForeground(Color.BLACK);
+        newPasswordField.setText("");
+        constraints = new GridBagConstraints();
+        constraints.gridx = 1;
+        constraints.gridy = 4;
+        constraints.insets = new Insets(10, 0, 10, 40);
+        panel.add(newPasswordField, constraints);
+        
+        // Crear boton "Actualizar Información"
+        JButton updateButton = new JButton("Actualizar Información");
+        updateButton.setPreferredSize(new Dimension(273, 38));
+        updateButton.setFont(new Font("Andale Mono", Font.PLAIN, 15));
+        updateButton.setBackground(Color.decode("#040404"));
+        updateButton.setForeground(Color.WHITE);
+        
+        UserController userController = new UserController();
+        updateButton.addActionListener((ActionEvent e) -> {
+            // Acciones para actualizar información
+            String updatedName = nameField.getText();
+            String updatedEmail = emailField.getText();
+            String updatedPhone = phoneField.getText();
+            String updatedPassword = newPasswordField.getText();
+            
+            
+            // Actualizar la información del usuario
+            User updatedUser = new User(
+                    RoomReservation.loggedInUser.getUserId(),
+                    updatedName,
+                    updatedEmail,
+                    updatedPhone,
+                    updatedPassword,
+                    RoomReservation.loggedInUser.getRole()
+            );
+            
+            userController.updateUser(updatedUser);
+            JOptionPane.showMessageDialog(null, "Información actualizadaluis con éxito!", "Éxito", JOptionPane.INFORMATION_MESSAGE);
+            RoomReservation.loggedInUser = updatedUser;
+            
+            // Redirect Home
+            new HomeJFrame().setVisible(true); 
+            dispose(); // Cierra el JFrame actual
+        });
+
+        constraints = new GridBagConstraints();
+        constraints.gridx = 1;
+        constraints.gridy = 5;
+        constraints.gridwidth = 1;
+        constraints.insets = new Insets(10, 0, 10, 40);
+        constraints.anchor = GridBagConstraints.EAST;
+        panel.add(updateButton, constraints);
+        
+        // Configurar el panel principal en el JFrame
+        setContentPane(panel); // Cambia el contenido del JFrame
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     /**
